bd2d19687e6e55542f9bfe90453c0db319eae763
dlopes7 <davidribeirolopes@gmail.com>
2024-10-14 16:43:33 -0500

    make api changes as in otel

--- netflowreceiver/factory.go
+++ netflowreceiver/factory.go
@@ -2,6 +2,7 @@ package netflowreceiver
 
 import (
 	"context"
+
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/receiver"
@@ -14,7 +15,7 @@ var (
 const (
 	defaultSockets   = 1
 	defaultWorkers   = 2
-	defaultQueueSize = 1_000_000
+	defaultQueueSize = 1_000
 )
 
 // NewFactory creates a factory for netflow receiver.
@@ -26,29 +27,18 @@ func NewFactory() receiver.Factory {
 }
 
 func createDefaultConfig() component.Config {
-	return &Config{
-		Listeners: []ListenerConfig{
-			{
-				Scheme:    "sflow",
-				Port:      6343,
-				Sockets:   defaultSockets,
-				Workers:   defaultWorkers,
-				QueueSize: defaultQueueSize,
-			},
-			{
-				Scheme:    "netflow",
-				Port:      2055,
-				Sockets:   defaultSockets,
-				Workers:   defaultWorkers,
-				QueueSize: defaultQueueSize,
-			},
-		},
+	return Config{
+		Scheme:    "netflow",
+		Port:      2055,
+		Sockets:   defaultSockets,
+		Workers:   defaultWorkers,
+		QueueSize: defaultQueueSize,
 	}
 }
 
 func createLogsReceiver(_ context.Context, params receiver.CreateSettings, cfg component.Config, consumer consumer.Logs) (receiver.Logs, error) {
 	logger := params.Logger
-	conf := cfg.(*Config)
+	conf := cfg.(Config)
 
 	nr := &netflowReceiver{
 		logger:      logger,
